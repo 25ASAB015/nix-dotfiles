9872cec9432a261064c373eccbd178b2a3459e8a
Roberto Flores <25asab015@ujmd.edu.sv>
2026-01-15 06:23:03 -0600

    fix: use symlinks for scripts without executable flag

--- modules/hm/files.nix
+++ modules/hm/files.nix
@@ -13,18 +13,12 @@ in {
     ".config/hypr/keybindings.conf" = lib.mkForce {
       source = mkSymlink "${dotfilesDir}/resources/config/keybinds.conf";
     };
-    ".local/lib/hyde/script_launcher.sh" = {
-      source = mkSymlink "${dotfilesDir}/resources/scripts/script_launcher.sh";
-      executable = true;
-    };
-    ".local/lib/hyde/record.sh" = {
-      source = mkSymlink "${dotfilesDir}/resources/scripts/record.sh";
-      executable = true;
-    };
-    ".local/lib/hyde/dict.sh" = {
-      source = mkSymlink "${dotfilesDir}/resources/scripts/dict.sh";
-      executable = true;
-    };
+    ".local/lib/hyde/script_launcher.sh".source =
+      mkSymlink "${dotfilesDir}/resources/scripts/script_launcher.sh";
+    ".local/lib/hyde/record.sh".source =
+      mkSymlink "${dotfilesDir}/resources/scripts/record.sh";
+    ".local/lib/hyde/dict.sh".source =
+      mkSymlink "${dotfilesDir}/resources/scripts/dict.sh";
   };
 }
 
