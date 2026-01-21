ead080155dc603757b6c2c57d9876e40659334c9
Roberto Flores <25asab015@ujmd.edu.sv>
2026-01-21 00:14:18 -0600

    style: remove hardcoded rofi theme from script_launcher

--- resources/scripts/script_launcher.sh
+++ resources/scripts/script_launcher.sh
@@ -33,81 +33,10 @@ for script in *.sh; do
     script_list+="$script\t$description\n"
 done
 
-# Nord color scheme
-nord_theme='
-* {
-    background-color: #2e3440;
-    text-color: #d8dee9;
-    border-color: #4c566a;
-    selected-normal-background: #5e81ac;
-    selected-normal-foreground: #eceff4;
-    alternate-normal-background: #3b4252;
-    alternate-normal-foreground: #d8dee9;
-    font: "JetBrainsMono Nerd Font 12";
-}
 
-window {
-    background-color: #2e3440;
-    border: 2px;
-    border-color: #4c566a;
-    border-radius: 8px;
-    padding: 10px;
-}
 
-inputbar {
-    background-color: #3b4252;
-    text-color: #d8dee9;
-    border-radius: 4px;
-    padding: 8px;
-    margin: 0px 0px 10px 0px;
-}
-
-prompt {
-    background-color: transparent;
-    text-color: #88c0d0;
-}
-
-entry {
-    background-color: transparent;
-    text-color: #d8dee9;
-    placeholder-color: #616e88;
-}
-
-listview {
-    background-color: transparent;
-    columns: 1;
-    lines: 10;
-    spacing: 2px;
-    scrollbar: true;
-}
-
-element {
-    background-color: transparent;
-    text-color: #d8dee9;
-    padding: 8px;
-    border-radius: 4px;
-}
-
-element selected {
-    background-color: #5e81ac;
-    text-color: #eceff4;
-}
-
-element alternate {
-    background-color: #3b4252;
-}
-
-scrollbar {
-    width: 4px;
-    border: 0;
-    handle-color: #4c566a;
-    handle-width: 4px;
-    padding: 0;
-}
-'
-
-# Show in rofi with Nord theme
-selected=$(echo -e "$script_list" | rofi -dmenu -p "Scripts:" -i -theme-str "$nord_theme")
+# Show in rofi with system theme
+selected=$(echo -e "$script_list" | rofi -dmenu -p " Scripts" -i)
 
 # Exit if nothing selected
 [[ -z "$selected" ]] && exit 0
