9bc919d7018e52bbcdba5380fa685a2cc7525f60
Roberto Flores <25asab015@ujmd.edu.sv>
2026-01-20 21:09:57 -0600

    config: update 2026-01-20 21:09:56

--- modules/hm/files.nix
+++ modules/hm/files.nix
@@ -10,9 +10,9 @@ let
 in {
   # Archivos mutables (se actualizan sin rebuild al editar en el repo)
   home.file = {
-    ".local/share/waybar/modules/custom-weather.jsonc" = lib.mkForce {
-      source = mkSymlink "${dotfilesDir}/resources/config/custom-weather.jsonc";
-    };
+    # ".local/share/waybar/modules/custom-weather.jsonc" = lib.mkForce {
+    #   source = mkSymlink "${dotfilesDir}/resources/config/custom-weather.jsonc";
+    # };
 
     ".config/hypr/keybindings.conf" = lib.mkForce {
       source = mkSymlink "${dotfilesDir}/resources/config/keybinds.conf";
@@ -47,6 +47,12 @@ in {
     if [ -d "${dotfilesDir}/resources/scripts" ]; then
       chmod -R u+rx,go+rx "${dotfilesDir}/resources/scripts"
     fi
+  '';
+
+  # Link weather config manually to ensure it points to the mutable file
+  home.activation.linkWeatherConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD mkdir -p $VERBOSE_ARG "${config.home.homeDirectory}/.local/share/waybar/modules"
+    $DRY_RUN_CMD ln -sf $VERBOSE_ARG "${dotfilesDir}/resources/config/custom-weather.jsonc" "${config.home.homeDirectory}/.local/share/waybar/modules/custom-weather.jsonc"
   '';
 }
 
