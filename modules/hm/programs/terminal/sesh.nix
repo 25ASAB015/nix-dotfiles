0a29181184ef477ff4ebc84f7777f9d3c865ecd4
Roberto Flores <25asab015@ujmd.edu.sv>
2026-01-30 07:38:35 -0600

    fix(sesh): relocate Zsh keybinding to Alt+g to avoid Ghostty conflict

--- modules/hm/programs/terminal/sesh.nix
+++ modules/hm/programs/terminal/sesh.nix
@@ -57,8 +57,8 @@
     }
 
     zle     -N             sesh-sessions
-    bindkey -M emacs '\es' sesh-sessions
-    bindkey -M vicmd '\es' sesh-sessions
-    bindkey -M viins '\es' sesh-sessions
+    bindkey -M emacs '\eg' sesh-sessions
+    bindkey -M vicmd '\eg' sesh-sessions
+    bindkey -M viins '\eg' sesh-sessions
   '';
 }
