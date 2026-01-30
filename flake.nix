a154d2d9858fe344d914f3f1a6a0d5031ed054af
Roberto Flores <25asab015@ujmd.edu.sv>
2026-01-29 20:22:25 -0600

    fix(flake): use nixpkgs.config in module instead of overriding pkgs to preserve hydenix packages

--- flake.nix
+++ flake.nix
@@ -70,35 +70,28 @@
   outputs =
     { ... }@inputs:
     let
-      # Import nixpkgs with configuration to allow unfree packages
-      pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        config = {
-          allowUnfreePredicate = pkg:
-            let
-              pkgName = inputs.nixpkgs.lib.getName pkg;
-            in
-            builtins.elem pkgName [
-              "antigravity"
-              "antigravity-fhs"
-              "code"
-              "code-fhs"
-              "code-cursor"
-              "code-cursor-fhs"
-              "vscode"
-              "vscode-fhs"
-            ];
-        };
-      };
-      
       # Main desktop PC configuration
       hydenixConfig = inputs.nixpkgs.lib.nixosSystem {
         # Modern syntax (replaces deprecated 'system')
         modules = [
-          { nixpkgs.hostPlatform = "x86_64-linux"; }
-          # Pass configured pkgs to override the externally created instance
-          {
-            nixpkgs.pkgs = pkgs;
+          { 
+            nixpkgs.hostPlatform = "x86_64-linux";
+            # Configure nixpkgs to allow unfree packages for editors
+            # This is done here because nixpkgs is created externally
+            nixpkgs.config.allowUnfreePredicate = pkg:
+              let
+                pkgName = inputs.nixpkgs.lib.getName pkg;
+              in
+              builtins.elem pkgName [
+                "antigravity"
+                "antigravity-fhs"
+                "code"
+                "code-fhs"
+                "code-cursor"
+                "code-cursor-fhs"
+                "vscode"
+                "vscode-fhs"
+              ];
           }
           ./hosts/hydenix/configuration.nix
         ];
